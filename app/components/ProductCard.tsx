ec9e8252cbd8afd4c12594712a55020eb21682b0
Hye Mee Park <hyemee1021@gmail.com>
2024-08-25 11:07:01 +1000

    css module

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import AddToCart from "./AddToCart";
+import styles from "./ProductCard.module.css";
+//style is an obj- i need to find a properety
 
 const ProductCard = () => {
   return (
-    <div>
+    <div className={styles.card}>
       <AddToCart />
     </div>
   );
